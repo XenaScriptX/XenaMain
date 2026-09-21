cceeab3cb11e4a417c3f784165f706bed89b3038
XenaScriptX <kenjieclark30@gmail.com>
2026-09-22 01:02:13 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,5 +1,5 @@
 local function s1() 
-    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/ehuvhIyDxB/0", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XenaScriptX/Best/refs/heads/main/Script.lua", true))()
 end 
 local function s2() 
     loadstring(game:HttpGet("https://cdn.sourceb.in/bins/K9o6kuh6Pc/0", true))()
